c5de2397713e92f02cab694e6104e7e940b202a7
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-17 13:33:50 -0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+import torchvision.models as models
+import torch
+from ptflops import get_model_complexity_info
+from model import ColorNet
+
+with torch.cuda.device(0):
+  net = model.ColorNet()
+  flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
+  print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
+  print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+
