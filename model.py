05d6a90d756e4ff5b3f5f6e38af90b88704a8c02
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-19 22:57:04 -0500

    Update model.py

--- model.py
+++ model.py
@@ -14,7 +14,9 @@ class ColorizationNet(nn.Module):
 
         # Convolutional layers and upsampling
         # self.deconv1_new = nn.ConvTranspose2d(midlevel_input_size, 128, kernel_size=4, stride=2, padding=1)
-        self.conv1 = nn.Conv2d(midlevel_input_size, 128, kernel_size=3, stride=1, padding=1)
+        self.conv0 = nn.Conv2d(global_input_size, 256, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(256)
+        self.conv1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
         self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(64)
@@ -27,15 +29,16 @@ class ColorizationNet(nn.Module):
 
         print('Loaded colorization net.')
 
-    def forward(self, midlevel_input): #, global_input):
+    def forward(self, global_output): #, global_input):
         
         # Convolutional layers and upsampling
-        x = F.relu(self.bn2(self.conv1(midlevel_input)))
+        x = F.relu(self.bn1(self.conv0(global_output)))
+        x = F.relu(self.bn2(self.conv1(x)))
         x = self.upsample(x)
         x = F.relu(self.bn3(self.conv2(x)))
         x = F.relu(self.conv3(x))
         x = self.upsample(x)
-        x = torch.sigmoid(self.conv4(x))
+        x = F.relu(self.conv4(x))
         x = self.upsample(self.conv5(x))
         return x
 
@@ -52,23 +55,23 @@ class ColorNet(nn.Module):
         # print(resnet_gray_model)
 
         # Only needed if not resuming from a checkpoint: load pretrained ResNet-gray model
-        # if torch.cuda.is_available(): # and only if gpu is available
-        #     resnet_gray_model.cuda()
-        #     # resnet_gray_weights = torch.load('pretrained/resnet_gray_weights.pth.tar') #torch.load('pretrained/resnet_gray.tar')['state_dict']
-        #     resnet_gray_model.load_state_dict(torch.load('pretrained/resnet_gray_weights.pth.tar'))
-        #     resnet_gray_model.cuda()
-        #     print('Pretrained ResNet-gray weights loaded')
+        if torch.cuda.is_available(): # and only if gpu is available
+            resnet_gray_model.cuda()
+            # resnet_gray_weights = torch.load('pretrained/resnet_gray_weights.pth.tar') #torch.load('pretrained/resnet_gray.tar')['state_dict']
+            resnet_gray_model.load_state_dict(torch.load('pretrained/resnet_gray_weights.pth.tar'))
+            resnet_gray_model.cuda()
+            print('Pretrained ResNet-gray weights loaded')
 
         # Extract midlevel and global features from ResNet-gray
-        self.midlevel_resnet = nn.Sequential(*list(resnet_gray_model.children())[0:6])
-        # self.global_resnet = nn.Sequential(*list(resnet_gray_model.children())[0:9])
+        # self.midlevel_resnet = nn.Sequential(*list(resnet_gray_model.children())[0:6])
+        self.global_resnet = nn.Sequential(*list(resnet_gray_model.children())[0:9])
         self.fusion_and_colorization_net = ColorizationNet()
 
     def forward(self, input_image):
 
         # Pass input through ResNet-gray to extract features
-        midlevel_output = self.midlevel_resnet(input_image)
-        # global_output = self.global_resnet(input_image)
+        # midlevel_output = self.midlevel_resnet(input_image)
+        global_output = self.global_resnet(input_image)
 
         # new_global_output = global_output.expand(-1, -1, 28, 28)
         # x = torch.cat((midlevel_output,new_global_output), 1)
@@ -76,5 +79,5 @@ class ColorNet(nn.Module):
         # print(midlevel_output.size())
         # print(new_global_output.size())
         # Combine features in fusion layer and upsample
-        output = self.fusion_and_colorization_net(midlevel_output) #, new_global_output)
+        output = self.fusion_and_colorization_net(global_output) #, new_global_output)
         return output
