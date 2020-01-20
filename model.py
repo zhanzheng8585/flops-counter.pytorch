61ee473e0c897daec76a3063dba48b64882e43d8
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-19 23:57:15 -0500

    Update model.py

--- model.py
+++ model.py
@@ -54,7 +54,6 @@ class ColorizationNet(nn.Module):
         # x = F.relu(self.bn1(self.deconv1_new(x)))
         # x = self.upsample(x)
         x = F.relu(self.bn2(self.conv1(x)))
-        x = self.upsample(x)
         # x = self.upsample(x)
         # x = F.relu(self.bn3(self.conv2(x)))
         # x = F.relu(self.bn4(self.conv3(x)))
@@ -65,6 +64,7 @@ class ColorizationNet(nn.Module):
         # x = F.relu(self.conv6(x))
         x = self.upsample(x)
         x = F.relu(self.conv7(x))
+        x = self.upsample(x)
         x = self.conv8(x)
         return x
 
