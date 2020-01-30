04af87c12d452aba602ba8e22659b173c3ad3ab1
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-29 21:46:44 -0500

    v1

--- example.py
+++ example.py
@@ -2,10 +2,11 @@ import torchvision.models as models
 import torch
 from ptflops import get_model_complexity_info
 import wdsr
-from option import args
+from option2 import parser
 
+args = parser.parse_args()
 with torch.cuda.device(6):
-	net = wdsr.MODEL(args)
+	net = wdsr.WDSR_B(args)
 	# net = models.densenet161()
 	flops, params = get_model_complexity_info(net, (3, 124, 118), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
--- wdsr.py
+++ wdsr.py
@@ -1,84 +1,52 @@
-import math
+from torch import nn
+from torch.nn.utils import weight_norm
+from .common import ShiftMean
 
-import torch
-import torch.nn as nn
-from torch.nn.parameter import Parameter
 
-class Block(nn.Module):
-    def __init__(
-        self, n_feats, kernel_size, wn, act=nn.ReLU(True), res_scale=1):
-        super(Block, self).__init__()
+class ResBlock(nn.Module):
+    def __init__(self, n_feats, expansion_ratio, res_scale=1.0, low_rank_ratio=0.8):
+        super(ResBlock, self).__init__()
         self.res_scale = res_scale
-        body = []
-        expand = 6
-        linear = 0.8
-        body.append(
-            wn(nn.Conv2d(n_feats, n_feats*expand, 1, padding=1//2)))
-        body.append(act)
-        body.append(
-            wn(nn.Conv2d(n_feats*expand, int(n_feats*linear), 1, padding=1//2)))
-        body.append(
-            wn(nn.Conv2d(int(n_feats*linear), n_feats, kernel_size, padding=kernel_size//2)))
-
-        self.body = nn.Sequential(*body)
+        self.module = nn.Sequential(
+            weight_norm(nn.Conv2d(n_feats, n_feats * expansion_ratio, kernel_size=1)),
+            nn.ReLU(inplace=True),
+            weight_norm(nn.Conv2d(n_feats * expansion_ratio, int(n_feats * low_rank_ratio), kernel_size=1)),
+            weight_norm(nn.Conv2d(int(n_feats * low_rank_ratio), n_feats, kernel_size=3, padding=1))
+        )
 
     def forward(self, x):
-        res = self.body(x) * self.res_scale
-        res += x
-        return res
+        return x + self.module(x) * self.res_scale
 
-class MODEL(nn.Module):
+
+class WDSR_B(nn.Module):
     def __init__(self, args):
-        super(MODEL, self).__init__()
-        # hyper-params
-        self.args = args
-        scale = args.scale[0]
-        n_resblocks = args.n_resblocks
-        n_feats = args.n_feats
-        kernel_size = 3
-        act = nn.ReLU(True)
-        # wn = lambda x: x
-        wn = lambda x: torch.nn.utils.weight_norm(x)
+        super(WDSR_B, self).__init__()
+        head = [weight_norm(nn.Conv2d(3, args.n_feats, kernel_size=3, padding=1))]
+        body = [ResBlock(args.n_feats, args.expansion_ratio, args.res_scale, args.low_rank_ratio)
+                for _ in range(args.n_res_blocks)]
+        tail = [weight_norm(nn.Conv2d(args.n_feats, 3 * (args.scale ** 2), kernel_size=3, padding=1)),
+                nn.PixelShuffle(args.scale)]
+        skip = [weight_norm(nn.Conv2d(3, 3 * (args.scale ** 2), kernel_size=5, padding=2)), nn.PixelShuffle(args.scale)]
 
-        self.rgb_mean = torch.autograd.Variable(torch.FloatTensor(
-            [args.r_mean, args.g_mean, args.b_mean])).view([1, 3, 1, 1])
-
-        # define head module
-        head = []
-        head.append(
-            wn(nn.Conv2d(args.n_colors, n_feats, 3, padding=3//2)))
-
-        # define body module
-        body = []
-        for i in range(n_resblocks):
-            body.append(
-                Block(n_feats, kernel_size, act=act, res_scale=args.res_scale, wn=wn))
-
-        # define tail module
-        tail = []
-        out_feats = scale*scale*args.n_colors
-        tail.append(
-            wn(nn.Conv2d(n_feats, out_feats, 3, padding=3//2)))
-        tail.append(nn.PixelShuffle(scale))
-
-        skip = []
-        skip.append(
-            wn(nn.Conv2d(args.n_colors, out_feats, 5, padding=5//2))
-        )
-        skip.append(nn.PixelShuffle(scale))
-
-        # make object members
         self.head = nn.Sequential(*head)
         self.body = nn.Sequential(*body)
         self.tail = nn.Sequential(*tail)
         self.skip = nn.Sequential(*skip)
 
+        self.subtract_mean = args.subtract_mean
+        self.shift_mean = ShiftMean(args.rgb_mean)
+
     def forward(self, x):
-        x = (x - self.rgb_mean.cuda()*255)/127.5
+        if self.subtract_mean:
+            x = self.shift_mean(x, mode='sub')
+
         s = self.skip(x)
         x = self.head(x)
         x = self.body(x)
         x = self.tail(x)
         x += s
-        x = x*127.5 + self.rgb_mean.cuda()*255
+
+        if self.subtract_mean:
+            x = self.shift_mean(x, mode='add')
+
         return x
