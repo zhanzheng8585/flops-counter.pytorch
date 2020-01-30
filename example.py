193b08f0406a3172b0db08accf32b5e950f7a900
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-01-29 21:32:18 -0500

    v1

--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 import torchvision.models as models
 import torch
 from ptflops import get_model_complexity_info
-import model2
+import edsr
+from option import args
 
 with torch.cuda.device(6):
-	net = model2.Generator(4)
+	net = edsr.EDSR(args)
 	# net = models.densenet161()
 	flops, params = get_model_complexity_info(net, (3, 124, 118), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
