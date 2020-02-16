d1ec84c7e572177e6f57f1bcb3ea8ff58f30672a
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-02-15 23:08:36 -0500

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ args = parser.parse_args()
 with torch.cuda.device(6):
 	net = wdsr.WDSR_B(args)
 	# net = models.densenet161()
-	flops, params = get_model_complexity_info(net, (3, 256, 256), as_strings=True, print_per_layer_stat=True)
+	flops, params = get_model_complexity_info(net, (3, 128, 128), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
 	print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
 	print('{:<30}  {:<8}'.format('Number of parameters: ', params))
