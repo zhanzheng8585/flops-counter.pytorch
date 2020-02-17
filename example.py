b8367b4e4be9fc7d6cb16bbfb7563742aef6fba7
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-02-16 22:51:06 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 import torchvision.models as models
 import torch
 from ptflops import get_model_complexity_info
-import wdsr
+import wdsr_b
 from option2 import parser
 
 args = parser.parse_args()
 with torch.cuda.device(6):
-	net = wdsr.WDSR_B(args)
+	net = wdsr_b.MODEL(args)
 	# net = models.densenet161()
 	flops, params = get_model_complexity_info(net, (3, 128, 128), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
