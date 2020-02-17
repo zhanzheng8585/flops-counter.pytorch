1b0b0a954561c2490c3e7d50e100a2b014b8ca5d
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-02-16 22:53:26 -0500

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ from args import *
 
 with torch.cuda.device(6):
 	args = get_args()
-	net = wdsr_b.MODEL(args)
+	net = MODEL(args)
 	# net = models.densenet161()
 	flops, params = get_model_complexity_info(net, (3, 128, 128), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
