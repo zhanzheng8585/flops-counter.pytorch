384504d8939e99e7f31f2502c9c45d7959a2cd2e
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-06-13 12:31:47 -0400

    v1

--- thop/profile.py
+++ thop/profile.py
@@ -132,6 +132,76 @@ def profile(model, inputs, rate, custom_ops=None, verbose=True):
 
     return total_ops, total_params
 
+def profile_ori(model, inputs, custom_ops=None, verbose=True):
+    handler_collection = []
+    if custom_ops is None:
+        custom_ops = {}
+
+    def add_hooks(m):
+        if len(list(m.children())) > 0:
+            return
+
+        if hasattr(m, "total_ops") or hasattr(m, "total_params"):
+            logger.warning("Either .total_ops or .total_params is already defined in %s. "
+                           "Be careful, it might change your code's behavior." % str(m))
+
+        m.register_buffer('total_ops', torch.zeros(1))
+        m.register_buffer('total_params', torch.zeros(1))
+
+        for p in m.parameters():
+            m.total_params += torch.Tensor([p.numel()])
+
+        m_type = type(m)
+        fn = None
+        if m_type in custom_ops:  # if defined both op maps, use custom_ops to overwrite.
+            fn = custom_ops[m_type]
+        elif m_type in register_hooks:
+            fn = register_hooks[m_type]
+
+        if fn is None:
+            if verbose:
+                logger.info("THOP has not implemented counting method for ", m)
+        else:
+            if verbose:
+                logger.info("Register FLOP counter for module %s" % str(m))
+            handler = m.register_forward_hook(fn)
+            handler_collection.append(handler)
+
+    training = model.training
+
+    model.eval()
+    model.apply(add_hooks)
+
+    with torch.no_grad():
+        model(*inputs)
+
+    total_ops = 0
+    total_params = 0
+    for m in model.modules():
+        if len(list(m.children())) > 0:  # skip for non-leaf module
+            continue
+        total_ops += m.total_ops
+        total_params += m.total_params
+
+    total_ops = total_ops.item()
+    total_params = total_params.item()
+
+    # reset model to original status
+    model.train(training)
+    for handler in handler_collection:
+        handler.remove()
+
+    # remove temporal buffers
+    for n, m in model.named_modules():
+        if len(list(m.children())) > 0:
+            continue
+        if "total_ops" in m._buffers:
+            m._buffers.pop("total_ops")
+        if "total_params" in m._buffers:
+            m._buffers.pop("total_params")
+
+    return total_ops, total_params
+
 
 def profile_2(model: nn.Module, inputs, custom_ops=None, verbose=True):
     handler_collection = {}
