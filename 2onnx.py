5d76f688b1cd1a663707979c8b66695230f23661
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-06-28 12:07:56 -0400

    Update 2onnx.py

--- 2onnx.py
+++ 2onnx.py
@@ -24,6 +24,88 @@ import matplotlib.pyplot as plt
 from skimage.color import lab2rgb
 from skimage import io
 
+import functools
+import torch.nn.functional as F
+
+
+def make_layer(block, n_layers):
+    layers = []
+    for _ in range(n_layers):
+        layers.append(block())
+    return nn.Sequential(*layers)
+
+
+class ResidualBlock_noBN(nn.Module):
+    '''Residual block w/o BN
+    ---Conv-ReLU-Conv-+-
+     |________________|
+    '''
+
+    def __init__(self, nf=64):
+        super(ResidualBlock_noBN, self).__init__()
+        self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+
+    def forward(self, x):
+        identity = x
+        out = F.relu(self.conv1(x), inplace=True)
+        out = self.conv2(out)
+        return identity + out
+
+
+class Vivo32ch2RBs(nn.Module):
+    def __init__(self, nf=32, nRBs=2):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+        self.sr = make_layer(RB_noBn, nRBs)
+        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+        self.pix_shuffle = nn.PixelShuffle(2)
+    
+    def forward(self, x):
+        y = self.relu(self.conv_in(x))
+        y = self.sr(y)
+        y = self.pix_shuffle(self.conv_out(y))
+
+        return y
+
+
+class Vivo16ch7RBs(nn.Module):
+    def __init__(self, nf=16, nRBs=7):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+        self.sr = make_layer(RB_noBn, nRBs)
+        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+        self.pix_shuffle = nn.PixelShuffle(2)
+    
+    def forward(self, x):
+        y = self.relu(self.conv_in(x))
+        y = self.sr(y)
+        y = self.pix_shuffle(self.conv_out(y))
+
+        return y
+
+
+class Vivo8ch29RBs(nn.Module):
+    def __init__(self, nf=16, nRBs=7):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+        self.sr = make_layer(RB_noBn, nRBs)
+        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+        self.pix_shuffle = nn.PixelShuffle(2)
+    
+    def forward(self, x):
+        y = self.relu(self.conv_in(x))
+        y = self.sr(y)
+        y = self.pix_shuffle(self.conv_out(y))
+
+        return y
+
 # import prune_util
 # from prune_util import GradualWarmupScheduler
 # from prune_util import CrossEntropyLossMaybeSmooth
@@ -33,7 +115,7 @@ from skimage import io
 # from model import ColorNet
 from wdsr_b import *
 from args import *
-from vivo import vivo
+# from vivo import vivo
 
 def main():
 
@@ -43,7 +125,7 @@ def main():
     # model = ColorNet()
     # args = get_args()
     # model = MODEL(args)
-    model = vivo.Vivo32ch2RBs()
+    model = Vivo32ch2RBs()
     # state_dict = torch.load("./checkpoint/checkpoint6/model_epoch133_step1.pth")
     # new_state_dict = OrderedDict()
 
@@ -76,7 +158,7 @@ def to_numpy(tensor):
 
 def check():
 
-    model = vivo.Vivo32ch2RBs()
+    model = Vivo32ch2RBs()
     checkpoint = torch.load("/home/zhanzheng/flops-counter.pytorch/vivo/vivo_32ch_2rbs.pth")
     model.load_state_dict(checkpoint)
 
