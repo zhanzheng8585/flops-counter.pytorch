845a437600ee412dd86a05773a0a91c9a840231c
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-06-28 11:04:06 -0400

    Update example.py

--- example.py
+++ example.py
@@ -5,12 +5,12 @@ from ptflops import get_model_complexity_info
 from option2 import parser
 from wdsr_b import *
 from args import *
-from vivo import *
+from vivo import vivo
 
 with torch.cuda.device(7):
 	args = get_args()
 	# net = WDSR_B(args)
-	net = Vivo32ch2RBs()
+	net = vivo.Vivo32ch2RBs()
 	# net = models.densenet161()
 	flops, params = get_model_complexity_info(net, (3, 960, 540), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
