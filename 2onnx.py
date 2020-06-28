67aab42b879e4bb9244dced9899d259349fc1f26
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-06-28 12:15:45 -0400

    v1

--- 2onnx.py
+++ 2onnx.py
@@ -28,83 +28,83 @@ import functools
 import torch.nn.functional as F
 
 
-def make_layer(block, n_layers):
-    layers = []
-    for _ in range(n_layers):
-        layers.append(block())
-    return nn.Sequential(*layers)
+# def make_layer(block, n_layers):
+#     layers = []
+#     for _ in range(n_layers):
+#         layers.append(block())
+#     return nn.Sequential(*layers)
 
 
-class ResidualBlock_noBN(nn.Module):
-    '''Residual block w/o BN
-    ---Conv-ReLU-Conv-+-
-     |________________|
-    '''
+# class ResidualBlock_noBN(nn.Module):
+#     '''Residual block w/o BN
+#     ---Conv-ReLU-Conv-+-
+#      |________________|
+#     '''
 
-    def __init__(self, nf=64):
-        super(ResidualBlock_noBN, self).__init__()
-        self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
-        self.conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+#     def __init__(self, nf=64):
+#         super(ResidualBlock_noBN, self).__init__()
+#         self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+#         self.conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
 
-    def forward(self, x):
-        identity = x
-        out = F.relu(self.conv1(x), inplace=True)
-        out = self.conv2(out)
-        return identity + out
+#     def forward(self, x):
+#         identity = x
+#         out = F.relu(self.conv1(x), inplace=True)
+#         out = self.conv2(out)
+#         return identity + out
 
 
-class Vivo32ch2RBs(nn.Module):
-    def __init__(self, nf=32, nRBs=2):
-        super().__init__()
-        self.relu = nn.ReLU()
-        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
-        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
-        self.sr = make_layer(RB_noBn, nRBs)
-        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
-        self.pix_shuffle = nn.PixelShuffle(2)
+# class Vivo32ch2RBs(nn.Module):
+#     def __init__(self, nf=32, nRBs=2):
+#         super().__init__()
+#         self.relu = nn.ReLU()
+#         self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+#         RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+#         self.sr = make_layer(RB_noBn, nRBs)
+#         self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+#         self.pix_shuffle = nn.PixelShuffle(2)
     
-    def forward(self, x):
-        y = self.relu(self.conv_in(x))
-        y = self.sr(y)
-        y = self.pix_shuffle(self.conv_out(y))
+#     def forward(self, x):
+#         y = self.relu(self.conv_in(x))
+#         y = self.sr(y)
+#         y = self.pix_shuffle(self.conv_out(y))
 
-        return y
+#         return y
 
 
-class Vivo16ch7RBs(nn.Module):
-    def __init__(self, nf=16, nRBs=7):
-        super().__init__()
-        self.relu = nn.ReLU()
-        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
-        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
-        self.sr = make_layer(RB_noBn, nRBs)
-        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
-        self.pix_shuffle = nn.PixelShuffle(2)
+# class Vivo16ch7RBs(nn.Module):
+#     def __init__(self, nf=16, nRBs=7):
+#         super().__init__()
+#         self.relu = nn.ReLU()
+#         self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+#         RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+#         self.sr = make_layer(RB_noBn, nRBs)
+#         self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+#         self.pix_shuffle = nn.PixelShuffle(2)
     
-    def forward(self, x):
-        y = self.relu(self.conv_in(x))
-        y = self.sr(y)
-        y = self.pix_shuffle(self.conv_out(y))
+#     def forward(self, x):
+#         y = self.relu(self.conv_in(x))
+#         y = self.sr(y)
+#         y = self.pix_shuffle(self.conv_out(y))
 
-        return y
+#         return y
 
 
-class Vivo8ch29RBs(nn.Module):
-    def __init__(self, nf=16, nRBs=7):
-        super().__init__()
-        self.relu = nn.ReLU()
-        self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
-        RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
-        self.sr = make_layer(RB_noBn, nRBs)
-        self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
-        self.pix_shuffle = nn.PixelShuffle(2)
+# class Vivo8ch29RBs(nn.Module):
+#     def __init__(self, nf=16, nRBs=7):
+#         super().__init__()
+#         self.relu = nn.ReLU()
+#         self.conv_in = nn.Conv2d(1, nf, 3, 1, 1)
+#         RB_noBn = functools.partial(ResidualBlock_noBN, nf=nf)
+#         self.sr = make_layer(RB_noBn, nRBs)
+#         self.conv_out = nn.Conv2d(nf, 4, 3, 1, 1)
+#         self.pix_shuffle = nn.PixelShuffle(2)
     
-    def forward(self, x):
-        y = self.relu(self.conv_in(x))
-        y = self.sr(y)
-        y = self.pix_shuffle(self.conv_out(y))
+#     def forward(self, x):
+#         y = self.relu(self.conv_in(x))
+#         y = self.sr(y)
+#         y = self.pix_shuffle(self.conv_out(y))
 
-        return y
+#         return y
 
 # import prune_util
 # from prune_util import GradualWarmupScheduler
@@ -115,7 +115,7 @@ class Vivo8ch29RBs(nn.Module):
 # from model import ColorNet
 from wdsr_b import *
 from args import *
-# from vivo import vivo
+from models import *
 
 def main():
 
