7310be977889cb8580d4ef600e0e9160c07243c3
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-06-28 11:58:48 -0400

    Update 2onnx.py

--- 2onnx.py
+++ 2onnx.py
@@ -33,6 +33,7 @@ from skimage import io
 # from model import ColorNet
 from wdsr_b import *
 from args import *
+from vivo import vivo
 
 def main():
 
@@ -40,8 +41,9 @@ def main():
     # Create model  
     # models.resnet18(num_classes=365)
     # model = ColorNet()
-    args = get_args()
-    model = MODEL(args)
+    # args = get_args()
+    # model = MODEL(args)
+    model = vivo.Vivo32ch2RBs()
     # state_dict = torch.load("./checkpoint/checkpoint6/model_epoch133_step1.pth")
     # new_state_dict = OrderedDict()
 
@@ -51,13 +53,14 @@ def main():
 
     # model = torch.nn.DataParallel(model)
     # model.load_state_dict(new_state_dict)
-    checkpoint = torch.load("./checkpoint/checkpoint6/model_epoch133_step1.pth")
-    model.load_state_dict(checkpoint["model"].state_dict())
+    checkpoint = torch.load("./vivo/vivo_32ch_2rbs.pth")
+    model.load_state_dict(checkpoint)
 
     # print(model)
-    input_shape = (1, 256, 256)
-    model_onnx_path = "./wdsr_b.onnx"
+    input_shape = (1, 960, 540)
+    model_onnx_path = "./Vivo32ch2RBs.onnx"
     model.train(False)
+    model.eval()
 
     # Export the model to an ONNX file
     dummy_input = Variable(torch.randn(1, *input_shape))
@@ -67,17 +70,46 @@ def main():
                               verbose=False)
     print("Export of torch_model.onnx complete!")
 
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
 
 def check():
 
-    # Load the ONNX model
-    model = onnx.load("wdsr_b.onnx")
+    model = vivo.Vivo32ch2RBs()
+    checkpoint = torch.load("./vivo/vivo_32ch_2rbs.pth")
+    model.load_state_dict(checkpoint)
 
-    # Check that the IR is well formed
-    onnx.checker.check_model(model)
+    # torch.nn.utils.remove_weight_norm(model.head[0])
+    # for i in range(1):
+    #     for j in [0,2,3]:
+    #         torch.nn.utils.remove_weight_norm(model.body[i].body[j])
+    # torch.nn.utils.remove_weight_norm(model.tail[0])
+    # torch.nn.utils.remove_weight_norm(model.skip[0])
 
-    # Print a human readable representation of the graph
-    onnx.helper.printable_graph(model.graph)
+    model.eval()
+    ort_session = onnxruntime.InferenceSession("Vivo32ch2RBs.onnx")
+
+    x = torch.randn(1, 1, 960, 540, requires_grad=False)
+    torch_out = model(x)
+    # # Load the ONNX model
+    # model = onnx.load("wdsr_b.onnx")
+
+    # # Check that the IR is well formed
+    # onnx.checker.check_model(model)
+
+    # # Print a human readable representation of the graph
+    # onnx.helper.printable_graph(model.graph)
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    # compare ONNX Runtime and PyTorch results
+    np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+
 
 if __name__ == '__main__':
     main()
