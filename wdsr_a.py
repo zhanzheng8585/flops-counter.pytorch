bf0f5d8f6dfa58e6e421ebec94b573fc7c1d75e1
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-07-08 18:53:39 -0400

    Update wdsr_a.py

--- wdsr_a.py
+++ wdsr_a.py
@@ -34,7 +34,7 @@ class WDSR_A(nn.Module):
         super(WDSR_A, self).__init__()
         # hyper-params
         self.args = args
-        scale = args.scale[0]
+        scale = args.scale
         n_resblocks = args.n_resblocks
         n_feats = args.n_feats
         kernel_size = 3
