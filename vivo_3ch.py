de77c7a45446e5fd611d4395ce7b62934bd1308b
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-08-31 18:07:57 -0400

    Update vivo_3ch.py

--- vivo_3ch.py
+++ vivo_3ch.py
@@ -75,7 +75,7 @@ def de_subpix(y):
 class Vivo3ch(nn.Module):
     def __init__(self, args):
         super().__init__()
-        scale = args.scale[0]
+        scale = args.scale
         nf = args.n_feats
         nRBs = args.n_resblocks
         self.relu = nn.ReLU()
