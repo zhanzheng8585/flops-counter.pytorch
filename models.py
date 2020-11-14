77dfd3a88b9cedb99b48d64746cf291968936e08
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-13 22:50:10 -0500

    v1

--- models.py
+++ models.py
@@ -28,9 +28,9 @@ class MLP(nn.Module):
 class CNNMnist(nn.Module):
     def __init__(self, args):
         super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
-        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         # self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(64 * 5 * 5, 512)
         self.fc2 = nn.Linear(512, args.num_classes)
