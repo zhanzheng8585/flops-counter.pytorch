d03d2f6b4a0d828c733400b58b5517977e55f8de
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-11-13 22:50:30 -0500

    v1

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ with torch.cuda.device(7):
 	net = CNNMnist(args=args)
 	# net = vivo.Vivo8ch29RBs()
 	# net = models.densenet161()
-	flops, params = get_model_complexity_info(net, (3, 32, 32), as_strings=True, print_per_layer_stat=True)
+	flops, params = get_model_complexity_info(net, (1, 32, 32), as_strings=True, print_per_layer_stat=True)
 	# flops, params = get_model_complexity_info(net, (3, 224, 224), as_strings=True, print_per_layer_stat=True)
 	print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
 	print('{:<30}  {:<8}'.format('Number of parameters: ', params))
--- models.py
+++ models.py
@@ -28,7 +28,7 @@ class MLP(nn.Module):
 class CNNMnist(nn.Module):
     def __init__(self, args):
         super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         # self.conv2_drop = nn.Dropout2d()
