706bb0ccbf1d59f9f123dbfbe3280a1110c554c6
zhanzheng8585 <zhanzheng8585@gmail.com>
2020-12-13 22:40:16 -0500

    v1

--- args.py
+++ args.py
@@ -9,7 +9,7 @@ def get_args():
     parser.add_argument('--n_resblocks', default=16,
                         help='Number of Residual Blocks')
 
-    parser.add_argument('--n_feats', default=28,
+    parser.add_argument('--n_feats', default=32,
                         help='Feature maps in the Residual Block mapping path way' +
                         'The total Parameter of the network is:' +
                         '(x*x*6*1 + 6*x*x*0.8 + 3*3*x*x*0.8)*5 + 27*x + 25*12*3')
